e8718459c9c4c4681d0a3a8655d37c6f2c37deea
YuvanX <abhivigneshofficial@gmail.com>
2025-08-14 11:34:14 +0530

    feat: add api to create post

---
+++ app/api/create/route.ts
@@ -0,0 +1,28 @@
+import prisma from "@/db/src";
+import { Tag } from "@/db/src/generated/prisma";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function POST(req: NextRequest) {
+    try {
+        const body = await req.json();
+        const { title, description, tag } = body;
+
+        if(!title?.trim() || !description?.trim() || !tag) {
+            return NextResponse.json({ message: "Please Provide valid details"}, { status: 401 })
+        }
+
+        await prisma.post.create({
+            data: {
+                title: title.trim(),
+                description: description.trim(),
+                tag: tag === "idea" ? Tag.Idea : Tag.Issue 
+            }
+        })
+
+        return NextResponse.json({ message: "Created a post successfully"}, { status: 200 })
+
+    } catch (error) {
+        console.log(error)
+        return NextResponse.json({ message: "Internal server error"}, { status: 500 })
+    }
+}
