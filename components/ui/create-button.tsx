9a8005284cc0d14bbcc10fc455dc0b7c2a6e6ef5
YuvanX <abhivigneshofficial@gmail.com>
2025-08-15 08:05:14 +0530

    fix: make client

--- components/ui/create-button.tsx
+++ components/ui/create-button.tsx
@@ -1,8 +1,12 @@
+"use client"
 import { Plus } from "lucide-react"
 import { Button } from "./button"
+import { useRouter } from "next/navigation"
 
 export const CreateButton = () => {
-    return <Button className="w-10 h-10 rounded-full bg-green-500  flex items-center justify-center cursor-pointer">
+    const router = useRouter();
+
+    return <Button onClick={() => router.push("/create")} className="w-10 h-10 rounded-full bg-green-500  flex items-center justify-center cursor-pointer">
         <Plus />
     </Button>
 }
