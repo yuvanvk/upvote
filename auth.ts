2495e68830e1a7ae66ddb05e29d92ab5d6b5956a
YuvanX <abhivigneshofficial@gmail.com>
2025-08-15 16:00:56 +0530

    feat: auth lib

---
+++ auth.ts
@@ -0,0 +1,23 @@
+import NextAuth from "next-auth";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import prisma from "./db/src";
+import Google from "next-auth/providers/google";
+
+export const { handlers, auth, signIn, signOut } = NextAuth({
+  adapter: PrismaAdapter(prisma),
+  providers: [Google],
+  session: {
+    strategy: "database",
+  },
+  pages: {
+    signIn: "/"
+  },
+  callbacks: {
+    async session({ session, user }: any) {
+      if (session.user) {
+        session.user.id = user.id;
+      }
+      return session;
+    },
+  },
+});
