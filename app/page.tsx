62215b2e44e6bd2b97c6d20456489085e9c82d90
YuvanX <abhivigneshofficial@gmail.com>
2025-09-18 18:54:16 +0530

    checkpoint

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,11 @@ export default async function Home() {
   return (
     <div>
       <Navbar />
-      <div className="mx-auto max-w-7xl bg-[#202020]"></div>
+      <div className="mx-auto max-w-7xl ">
+        <div className="pt-28">
+          <div className="text-2xl font-semibold tracking-wide capitalize">Top ideas today</div>
+        </div>
+      </div>
     </div>
   );
 }
